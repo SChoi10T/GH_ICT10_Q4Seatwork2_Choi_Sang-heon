f9b9f226dab8a4aa55a5725c4a25519f0b25e4ea
Sang-heon <choi.sg@obmontessori.edu.ph>
2026-04-06 08:54:56 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+# My Classmates
+from pyscript import display, document # pyright: ignore[reportMissingImports]
+
+
+class Classmate:
+    def __init__(self, name, section, favorite_subject):
+        self.name = name
+        self.section = section
+        self.favorite_subject = favorite_subject
+
+    # Introduce Method
+    def introduce(self):
+        return(f"Hello, I am {self.name}, I'm from {self.section}, and I like {self.favorite_subject}") # Default Message
+
+# Objects of Class: Classmate
+c1 = Classmate("Sean", "Topaz", "PE")
+c2 = Classmate("Allen", "Topaz", "ICT")
+c3 = Classmate("Ramon", "Topaz", "TLE")
+c4 = Classmate("Jalainie", "Topaz", "Math")
+c5 = Classmate("Calvin", "Topaz", "English")
+
+# List of Classmates
+classmates = [c1, c2, c3, c4, c5]
+
+# Show List of Classmates
+def show_list(e):
+    document.getElementById('output').innerHTML = "" # Clear Output
+
+    # Looping Introductions for each Object
+    for i in classmates:
+        display(i.introduce(), target="output") 
+
+# Add Classmate towards the List
+def add_classmate(e):
+    document.getElementById('output').innerHTML = "" # Clear Output
+
+    # Input values
+    name = document.getElementById('name').value
+    section = document.getElementById('section').value
+    favorite_subject = document.getElementById('subject').value
+
+    new_member = Classmate(name, section, favorite_subject)
+    classmates.append(new_member)
+
+    if new_member:
+        display("Classmate added, click Show List again.", target="output")
+    else:
+        show_list
